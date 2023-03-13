a5845bf7fe81bfe746c61ea482f840ba17b1f143
Viktoriia_Bielik <byelik09@gmail.com>
2023-03-13 11:31:50 +0200

    home work 2

---
+++ second_lesson/Bielik_Viktoriia.py
@@ -0,0 +1,8 @@
+print("Solve the quadratic equation")
+
+a = int(input('Enter a= '))
+b = int(input('Enter b= '))
+c = int(input('Enter c= '))
+
+Desc = b ** 2 - 4 * a * c
+print ('Solve D  = ', Desc)
