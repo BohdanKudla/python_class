8391366d8b1f90d35ec859eb5b3c94ee7db14a56
Serhii Khristov <serega.khristov17@gmail.com>
2023-03-13 19:16:22 +0200

    Home Task 1

---
+++ second_lesson/Serhii_Khristov1.py
@@ -0,0 +1,8 @@
+a = float(input("Add 'a' variable = "))
+b = float(input("Add 'b' variable = "))
+c = float(input("Add 'c' variable = "))
+discriminant = (b ** 2) - (4 * a * c)
+print('ax^2+bx+c = 0\n')
+print(discriminant)
+print('X1 = ', ((-b + (discriminant ** 0.5)) / (2 * a)), '\n')
+print('X2 = ', ((-b - (discriminant ** 0.5)) / (2 * a)))
