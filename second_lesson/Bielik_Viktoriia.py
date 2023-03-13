b71eb638ba8af8fdf303efe610939386b4a5fa7f
Viktoriia_Bielik <byelik09@gmail.com>
2023-03-13 16:00:58 +0200

    some changes

--- second_lesson/Bielik_Viktoriia.py
+++ second_lesson/Bielik_Viktoriia.py
@@ -1,3 +1,5 @@
+import math
+
 print("Solve the quadratic equation")
 
 a = int(input('Enter a= '))
@@ -5,4 +7,12 @@ b = int(input('Enter b= '))
 c = int(input('Enter c= '))
 
 Desc = b ** 2 - 4 * a * c
+
 print ('Solve D  = ', Desc)
+if Desc > 0:
+    x1 = (-b + math.sqrt(Desc)) / (2 * a)
+    x1 = (-b - math.sqrt(Desc)) / (2 * a)
+elif Desc == 0:
+    x = -b / (2 * a)
+else:
+    print('No roots')
