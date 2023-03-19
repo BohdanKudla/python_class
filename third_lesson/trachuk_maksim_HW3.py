da3400b01551142a23683191f43857be34c99832
Lenovo <tracukmaksim@gmail.com>
2023-03-19 13:33:39 +0200

    Retake home task 3

--- third_lesson/trachuk_maksim_HW3.py
+++ third_lesson/trachuk_maksim_HW3.py
@@ -1,23 +1,23 @@
 my_list = []
-num = int(input("Какое количество чисел вы хотите? "))
+try:
+    user = input("Введите список чисел: ")
+
+
+    my_list = [int(num) for num in user.split()]
 
 
 
-for ele in range(num):
-
-    try:
-        number = int(input("Введите число ="))
-        my_list.append(number)
-
-    except ValueError:
-        print("ERROR: Вы ввели не число. Попробуйте еще раз")
 
 
 
-minimum = my_list[0]
 
-for min_num in my_list:
+    minimum = my_list[0]
+
+    for min_num in my_list:
         if minimum >= min_num:
             minimum = min_num
 
-print(f"Минимальное число {minimum}")
+
+    print(f"Минимальное число {minimum}")
+except ValueError:
+    print("ERROR:Вы ввели не верные данные")
