dd469ffbf4968b960a0e0aeb2cee139b004ba8f3
Lenovo <tracukmaksim@gmail.com>
2023-03-19 14:02:12 +0200

    Retake home task 5

--- third_lesson/trachuk_maksim_HW5.py
+++ third_lesson/trachuk_maksim_HW5.py
@@ -2,14 +2,13 @@
 try:
 
     n = int(input("Введите число: "))
-    count = 0
+    count = 1
 
-    if n == 0:
-        count = 1
-    else:
-        while n > 0:
-            count += 1
-            n //= 10
+    while n >= 10:
+        n = n // 10
+        count += 1
+
+
     print(f"Количество цифр в числе {count}")
 
 except ValueError:
