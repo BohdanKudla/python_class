118ae46e82a18fd8875ad0fad4625d2fcee17eb4
Lenovo <tracukmaksim@gmail.com>
2023-03-19 13:41:43 +0200

    Retake home task 4

--- third_lesson/trachuk_maksim_HW4.py
+++ third_lesson/trachuk_maksim_HW4.py
@@ -1,8 +1,9 @@
 try:
     number = int(input('Введите число: '))
     if 10 <= number <= 99:
-        print(number // 10, number % 10)
-    else:
-        print('Неверный ввод')
+        first = number //  10
+        second = number % 10
+        print(first, second)
+
 except ValueError:
     print('Неверный ввод')
