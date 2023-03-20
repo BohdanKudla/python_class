c8df16260596cd611e88fd65079b47beef213fbf
f00ntik <fyntikov2502@gmail.com>
2023-03-20 06:23:45 +0200

    getting rid of 'else' block

--- fourth_lesson/oleksandr_funtykov_7.py
+++ fourth_lesson/oleksandr_funtykov_7.py
@@ -6,6 +6,5 @@ try:
         int(number)
 except ValueError:
     print("Последовательность должна состоять только из чисел")
-else:
-    sum_even = sum(int(numbers_list[i]) for i in range(0, len(numbers_list), 2))
-    print(f"Сумма элементов с четными индексами: {sum_even}")
+
+    print(f"Сумма элементов с четными индексами: {sum(int(numbers_list[i]) for i in range(0, len(numbers_list), 2))}")
